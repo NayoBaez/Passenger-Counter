c278905973411f0282046cec91e700f6f1c67198
NayoBaez <104393006+NayoBaez@users.noreply.github.com>
2022-11-03 17:33:53 -0400

    Added Buttons with css

---
+++ index.js
@@ -0,0 +1,19 @@
+// let myAge = 32;
+// let humanDogRatio = 7;
+// let myDogAge = myAge * humanDogRatio;
+
+// console.log(myDogAge);
+
+let bonusPoints = 50;
+
+bonusPoints = bonusPoints + 50;
+
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints - 75;
+
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints + 45;
+
+console.log(bonusPoints);
